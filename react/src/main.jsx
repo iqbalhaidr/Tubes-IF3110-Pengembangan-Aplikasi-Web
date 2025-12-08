d2318035a0b560eb11837d43973bb74fb0a6c28d
rafifrs <rafiffarras023@gmail.com>
2025-12-08 13:39:12 +0700

    feat: integrate push to main page

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -6,6 +6,19 @@ import { SocketProvider } from './contexts/SocketContext.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import './index.css'
 
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/chat/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('Service Worker registration failed: ', registrationError);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
